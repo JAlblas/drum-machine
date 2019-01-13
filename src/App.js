d98d2a388b6f0d0f193afa581af7d170f29ab0d0
Jasper Alblas <just_jasper@hotmail.com>
2019-01-13 20:02:32 +0100

    Initial project files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import DrumMachine from "./DrumMachine.js";
+
+class App extends Component {
+  render() {
+    return (
+      <DrumMachine/>
+    );
+  }
+}
+
+export default App;
---
+++ src/DrumMachine.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class DrumMachine extends Component {
+  render() {
+    return (
+    <div id="wrapper">
+      <div id="display">
+      </div>
+      <div id="buttons">
+        <div className="drum-pad">
+          Test
+        </div>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default DrumMachine;
